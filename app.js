eff08fc7c39c5f0f660688c9960d625c834d7c92
LEXINFINTECH\aislingxie <aislingxie@lexinfintech.com>
2019-05-16 19:10:47 +0800

    add allow origin :japanese_castle

--- app.js
+++ app.js
@@ -42,4 +42,12 @@ app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 });
 
+//  允许跨域
+app.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*'); // 很奇怪的是，使用 * 会出现一些其他问题
+  ctx.set('Access-Control-Allow-Headers', 'content-type');
+  ctx.set('Access-Control-Allow-Methods', 'OPTIONS,GET,HEAD,PUT,POST,DELETE,PATCH')
+  await next();
+});
+
 module.exports = app
